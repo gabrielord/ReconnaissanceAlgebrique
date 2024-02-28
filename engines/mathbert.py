c97ed0bff8ff0184fdd5d6067e4fb19ed6c316e5
AncientPatata <abde.arrahman.dk@gmail.com>
2024-02-28 14:00:13 +0100

    Web page and other misc

---
+++ engines/mathbert.py
@@ -0,0 +1,3 @@
+#https://huggingface.co/tbs17/MathBERT
+#https://arxiv.org/abs/2105.00377
+# TODO: Look into it later 
--- ra-webui/src/App.js
+++ ra-webui/src/App.js
@@ -1,6 +1,19 @@
-import { Box, Button, ChakraProvider, Flex, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Flex,
+  Input,
+  Select,
+  Spacer,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import "katex/dist/katex.min.css";
+import { BlockMath } from "react-katex";
 
 function App() {
+  const [leftExpr, setLeftExpr] = useState("");
+  const [rightExpr, setRightExpr] = useState("");
   return (
     <ChakraProvider>
       <Flex flexDir="column" alignItems="center">
@@ -13,7 +26,7 @@ function App() {
               borderWidth="1px"
               borderColor="gray.200"
             >
-              <Flex flexDir="column" width="100%">
+              <Flex flexDir="column" width="100%" gap="20px">
                 <Box
                   fontSize="2xl"
                   fontWeight="thin"
@@ -21,8 +34,72 @@ function App() {
                   textAlign="center"
                   width="100%"
                 >
-                  Reconnaissance Algebrique
+                  Reconnaissance Algebrique (hasn't been styled yet... todo)
                 </Box>
+                <Box
+                  display="flex"
+                  flexDir="row"
+                  width="100%"
+                  alignContents="center"
+                >
+                  <Box marginLeft="50px" marginRight="50px" textAlign="center">
+                    Method
+                  </Box>
+                  <Select placeholder="Select option">
+                    {" "}
+                    {/**Get options from server */}
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                  </Select>
+                </Box>
+
+                <Box
+                  display="flex"
+                  flexDir="row"
+                  width="100%"
+                  alignContents="center"
+                >
+                  <Box
+                    paddingLeft="50px"
+                    paddingRight="50px"
+                    alignContent="center"
+                  >
+                    Left Expression
+                  </Box>
+                  <Input
+                    value={leftExpr}
+                    onChange={(e) => setLeftExpr(e.target.value)}
+                  />
+                </Box>
+                <Box
+                  display="flex"
+                  flexDir="row"
+                  width="100%"
+                  alignContents="center"
+                >
+                  <Box
+                    paddingLeft="50px"
+                    paddingRight="50px"
+                    alignContent="center"
+                  >
+                    Right Expression
+                  </Box>
+                  <Input
+                    value={rightExpr}
+                    onChange={(e) => setRightExpr(e.target.value)}
+                  />
+                </Box>
+                <Box fontWeight="bold">Preview</Box>
+                <Box fontWeight="bold">Left expression : </Box>
+                <Box>
+                  <BlockMath>{leftExpr}</BlockMath>
+                </Box>
+                <Box fontWeight="bold">Right expression : </Box>
+                <Box>
+                  <BlockMath>{rightExpr}</BlockMath>
+                </Box>
+                <Button></Button>
               </Flex>
             </Box>
             <Box
